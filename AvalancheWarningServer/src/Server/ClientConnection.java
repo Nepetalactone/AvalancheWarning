065674cfb653cef3c3b8e3b7f00347895472ca54
Knoperaune <hubert.rall@students.fhv.at>
2013-03-05 15:48:01 +0200

    diagrams / doku

--- AvalancheWarningServer/src/Server/ClientConnection.java
+++ AvalancheWarningServer/src/Server/ClientConnection.java
@@ -1,16 +1,11 @@
 package Server;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
